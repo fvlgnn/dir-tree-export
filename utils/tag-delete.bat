53ddb11e5e5faaae8fe33649bd22ccfc63908d5f
Gianni F. <fvlgnn@gmail.com>
2025-05-06 14:27:18 +0200

    miglioramenti su script utils

--- utils/tag-delete.bat
+++ utils/tag-delete.bat
@@ -1,11 +1,11 @@
 @echo off
 
-REM NOTE: Questo script semplifica la cancellazione di un tag.
-REM tag-delete.bat
+:: NOTE: Questo script semplifica la cancellazione di un tag.
+:: tag-delete.bat
 
 set /p VERSION=Dichiara la versione (es. 1.2.3): 
 
-REM Esegue i comandi git
+:: Esegue i comandi git
 echo Cancellazione locale del tag v%VERSION%...
 git tag -d v%VERSION%
 
@@ -14,5 +14,4 @@ git push origin --delete v%VERSION%
 
 echo Tag v%VERSION% cancellato. Se collegata una release ricorda di cancellarla sul repository GitHub!
 
-@REM pause
 exit /b 0
--- utils/tag-release.bat
+++ utils/tag-release.bat
@@ -1,11 +1,11 @@
 @echo off
 
-REM NOTE: Questo script crea e invia un tag Git su un repository.
-REM tag-release.bat
+:: NOTE: Questo script crea e invia un tag Git su un repository.
+:: tag-release.bat
 
 set /p VERSION=Inserisci la versione (es. 1.2.3): 
 
-REM Esegue i comandi git
+:: Esegue i comandi git
 echo Creazione del tag v%VERSION%...
 git tag v%VERSION%
 
@@ -14,5 +14,4 @@ git push origin v%VERSION%
 
 echo Tag v%VERSION% creato e inviato con successo!
 
-@REM pause
 exit /b 0
